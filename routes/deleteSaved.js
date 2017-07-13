0a090d5d9bba56bd97d3c232a8d371ec313db759
raminrayhani <rayhani.ramin@gmail.com>
2017-07-13 19:09:29 -0400

    First commit

---
+++ routes/deleteSaved.js
@@ -0,0 +1,17 @@
+// Require savedArticle and comment models
+var SavedArticle = require('../models/savedArticleModel');
+
+module.exports = function(req, res) {
+
+  // Remove saved article
+  SavedArticle
+    .remove({_id: req.params.id})
+    .exec(function(err,data) {
+      if (err) {
+      // console.log(err);
+      res.json({status: 'error'})
+    } else {
+      res.json({status: 'deleted'})
+    }
+  })
+}
